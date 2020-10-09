57e719f70b47bb029a98a173d46cc11d6c85c0c0
Jrohy <euvkzx@gmail.com>
2020-10-09 11:41:05 +0800

    优化buildx.sh

--- buildx.sh
+++ buildx.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+LATEST_TAG=`git tag|awk 'END {print}'`
+
 gox -output="docker/result/webssh_{{.OS}}_{{.Arch}}" -ldflags="-s -w" -os="linux"
 cd docker
+docker buildx build --platform linux/arm64,linux/amd64,linux/arm,linux/386,linux/ppc64le,linux/s390x -t jrohy/webssh:${LATEST_TAG} . --push
 docker buildx build --platform linux/arm64,linux/amd64,linux/arm,linux/386,linux/ppc64le,linux/s390x -t jrohy/webssh . --push
