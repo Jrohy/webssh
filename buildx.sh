c3aaf50f658f6349e85d7953b461b4d8fb8e6fdc
Jrohy <euvkzx@gmail.com>
2020-10-16 11:36:13 +0800

    fix buildx.sh

--- buildx.sh
+++ buildx.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 LATEST_TAG=`git describe --tags $(git rev-list --tags --max-count=1)`
 
+packr2
 gox -output="docker/result/webssh_{{.OS}}_{{.Arch}}" -ldflags="-s -w" -os="linux"
+packr2 clean
 cd docker
 docker buildx build --platform linux/arm64,linux/amd64,linux/arm,linux/386,linux/ppc64le,linux/s390x -t jrohy/webssh:${LATEST_TAG} . --push
 docker buildx build --platform linux/arm64,linux/amd64,linux/arm,linux/386,linux/ppc64le,linux/s390x -t jrohy/webssh . --push
