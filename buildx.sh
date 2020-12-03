2108ba9a862a4805d36d355e9ea4a3503bcb2894
Jrohy <euvkzx@gmail.com>
2020-12-03 11:37:05 +0800

    添加arm架构镜像标签推送

--- buildx.sh
+++ buildx.sh
@@ -7,3 +7,4 @@ packr2 clean
 cd docker
 docker buildx build --platform linux/arm64,linux/amd64,linux/arm,linux/386,linux/ppc64le,linux/s390x -t jrohy/webssh:${LATEST_TAG} . --push
 docker buildx build --platform linux/arm64,linux/amd64,linux/arm,linux/386,linux/ppc64le,linux/s390x -t jrohy/webssh . --push
+docker buildx build --platform linux/arm64 -t jrohy/webssh:arm64 . --push
