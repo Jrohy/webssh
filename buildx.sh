affa21d5259d34638352f7d1c5d321afe2ecac26
Jrohy <euvkzx@gmail.com>
2021-02-01 11:59:03 +0800

    优化编译脚本

--- buildx.sh
+++ buildx.sh
@@ -3,10 +3,10 @@ VERSION=`git describe --tags $(git rev-list --tags --max-count=1)`
 NOW=`TZ=Asia/Shanghai date "+%Y%m%d-%H%M"`
 GO_VERSION=`go version|awk '{print $3,$4}'`
 GIT_VERSION=`git rev-parse HEAD`
-LDFLAGS="-w -s -X 'main.version=version: $VERSION' -X 'main.buildDate=buildDate: $NOW' -X 'main.goVersion=goVersion: $GO_VERSION' -X 'main.gitVersion=gitVersion: $GIT_VERSION'"
+LDFLAGS="-w -s -X 'main.version=$VERSION' -X 'main.buildDate=$NOW' -X 'main.goVersion=$GO_VERSION' -X 'main.gitVersion=$GIT_VERSION'"
 
 packr2
-gox -output="docker/result/webssh_{{.OS}}_{{.Arch}}" -ldflags=$LDFLAGS -os="linux"
+gox -output="docker/result/webssh_{{.OS}}_{{.Arch}}" -ldflags="$LDFLAGS" -os="linux"
 packr2 clean
 cd docker
 docker buildx build --platform linux/arm64,linux/amd64,linux/arm,linux/386,linux/ppc64le,linux/s390x -t jrohy/webssh:${VERSION} . --push
