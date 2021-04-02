80a6fc600b4f96d2b95a0971aaed2459f290daa6
Jrohy <euvkzx@gmail.com>
2021-04-02 17:39:08 +0800

    升级前端版本

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,8 +1,5 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-    .BundleAnalyzerPlugin
 const path = require('path')
 const webpack = require('webpack')
-const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
 
 const proxyTargetMap = {
     prod: 'https://xxx.xxx.com/',
@@ -11,10 +8,13 @@ const proxyTargetMap = {
 const proxyTarget = proxyTargetMap[process.env.API_TYPE] || proxyTargetMap.prod
 const wsTarget = proxyTarget.replace('http', 'ws')
 const publicPath = process.env.NODE_ENV === 'production' ? '/' : '/'
-const dllPublishPath = publicPath === '/' ? '/vendor' : publicPath + '/vendor'
+
 module.exports = {
     publicPath: publicPath,
     outputDir: 'dist',
+
+    // 放置静态资源的地方 (js/css/img/font/...)
+    assetsDir: 'static',
 
     lintOnSave: true,
 
@@ -70,32 +70,6 @@ module.exports = {
             }
         },
         before: app => {}
-    },
-    // eslint-disable-next-line no-dupe-keys
-    configureWebpack: config => {
-        if (process.env.NODE_ENV === 'production') {
-            // 为生产环境修改配置...
-            config.plugins.push(
-                new webpack.DllReferencePlugin({
-                    context: process.cwd(),
-                    manifest: require('./public/vendor/vendor-manifest.json')
-                }),
-                // 将 dll 注入到 生成的 html 模板中
-                new AddAssetHtmlPlugin({
-                    // dll文件位置
-                    filepath: path.resolve(__dirname, './public/vendor/*.js'),
-                    // dll 引用路径
-                    publicPath: dllPublishPath,
-                    // dll最终输出的目录
-                    outputPath: './vendor'
-                })
-            )
-            if (process.env.npm_lifecycle_event === 'analyze') {
-                config.plugins.push(new BundleAnalyzerPlugin())
-            }
-        } else {
-            // 为开发环境修改配置...
-        }
     },
 
     // 第三方插件的选项
