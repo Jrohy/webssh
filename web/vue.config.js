4949a1d2067e85c16ff7e52f1ae7bc93287e877f
Jrohy <euvkzx@gmail.com>
2021-04-06 09:38:26 +0800

    优化前端编译打包

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,26 +1,18 @@
-const path = require('path')
-const webpack = require('webpack')
-
 const proxyTargetMap = {
     prod: 'https://xxx.xxx.com/',
     dev: 'http://127.0.0.1:5032'
 }
 const proxyTarget = proxyTargetMap[process.env.API_TYPE] || proxyTargetMap.prod
 const wsTarget = proxyTarget.replace('http', 'ws')
-const publicPath = process.env.NODE_ENV === 'production' ? '/' : '/'
 
 module.exports = {
-    publicPath: publicPath,
+    publicPath: '/',
     outputDir: 'dist',
 
     // 放置静态资源的地方 (js/css/img/font/...)
     assetsDir: 'static',
 
     lintOnSave: true,
-
-    transpileDependencies: [
-        /* string or regex */
-    ],
 
     // 是否为生产环境构建生成 source map？
     productionSourceMap: false,
@@ -35,10 +27,6 @@ module.exports = {
     // 在生产环境下为 Babel 和 TypeScript 使用 `thread-loader`
     // 在多核机器下会默认开启。
     parallel: require('os').cpus().length > 1,
-
-    // PWA 插件的选项。
-    // 查阅 https://github.com/vuejs/vue-docs-zh-cn/blob/master/vue-cli-plugin-pwa/README.md
-    pwa: {},
 
     // 配置 webpack-dev-server 行为。
     devServer: {
@@ -68,10 +56,13 @@ module.exports = {
                     '^/ws': ''
                 }
             }
-        },
-        before: app => {}
+        }
     },
-
-    // 第三方插件的选项
-    pluginOptions: {}
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            // 为生产环境修改配置...
+            config.optimization.splitChunks.maxSize = 500000
+            config.optimization.splitChunks.minSize = 300000
+        }
+    }
 }
