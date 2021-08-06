9d4dfafb1928c1acc7d1b451ee9333ecb08900c8
Jrohy <euvkzx@gmail.com>
2021-08-06 17:31:24 +0800

    fix bug

--- web/src/store/getters.js
+++ web/src/store/getters.js
@@ -5,7 +5,7 @@ export default {
             "ipaddress":"${state.sshInfo.host}", 
             "port":${state.sshInfo.port}, 
             "password":"${state.sshInfo.password.replace(/[\n]/g, '\\n')}",
-            "logintype":${state.sshInfo.logintype}
+            "logintype":${state.sshInfo.logintype === undefined ? 0 : state.sshInfo.logintype}
         }`
     )
 }
