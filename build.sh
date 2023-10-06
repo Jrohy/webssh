5ac4ddcbb0c405c8d209f39bb3f2767d0977399d
Jrohy <euvkzx@gmail.com>
2023-10-06 10:36:01 +0800

    优化编译脚本

--- build.sh
+++ build.sh
@@ -1,61 +1,61 @@
 #!/bin/bash
 
-GITHUB_TOKEN=""
+github_token=""
 
-PROJECT="Jrohy/webssh"
+project="Jrohy/webssh"
 
 #获取当前的这个脚本所在绝对路径
-SHELL_PATH=$(cd `dirname $0`; pwd)
+shell_path=$(cd `dirname $0`; pwd)
 
-RELEASE_ID=`curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/$PROJECT/releases/latest|grep id|awk 'NR==1{print $2}'|sed 's/,//'`
+release_id=`curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/$project/releases/latest|grep id|awk 'NR==1{print $2}'|sed 's/,//'`
 
 function uploadfile() {
-    FILE=$1
+    file=$1
 
-    CTYPE=$(file -b --mime-type $FILE)
+    ctype=$(file -b --mime-type $file)
 
-    curl -H "Authorization: token ${GITHUB_TOKEN}" -H "Content-Type: ${CTYPE}" --data-binary @$FILE "https://uploads.github.com/repos/$PROJECT/releases/${RELEASE_ID}/assets?name=$(basename $FILE)"
+    curl -H "Authorization: token ${github_token}" -H "Content-Type: ${ctype}" --data-binary @$file "https://uploads.github.com/repos/$project/releases/${release_id}/assets?name=$(basename $file)"
 
     echo ""
 }
 
 function upload() {
-    FILE=$1
-    DGST=$1.dgst
-    openssl dgst -md5 $FILE | sed 's/([^)]*)//g' >> $DGST
-    openssl dgst -sha1 $FILE | sed 's/([^)]*)//g' >> $DGST
-    openssl dgst -sha256 $FILE | sed 's/([^)]*)//g' >> $DGST
-    openssl dgst -sha512 $FILE | sed 's/([^)]*)//g' >> $DGST
-    uploadfile $FILE
-    uploadfile $DGST
+    file=$1
+    dgst=$1.dgst
+    openssl dgst -md5 $file | sed 's/([^)]*)//g' >> $dgst
+    openssl dgst -sha1 $file | sed 's/([^)]*)//g' >> $dgst
+    openssl dgst -sha256 $file | sed 's/([^)]*)//g' >> $dgst
+    openssl dgst -sha512 $file | sed 's/([^)]*)//g' >> $dgst
+    uploadfile $file
+    uploadfile $dgst
 }
 
-VERSION=`git describe --tags $(git rev-list --tags --max-count=1)`
-NOW=`TZ=Asia/Shanghai date "+%Y%m%d-%H%M"`
-GO_VERSION=`go version|awk '{print $3,$4}'`
-GIT_VERSION=`git rev-parse HEAD`
-LDFLAGS="-w -s -X 'main.version=$VERSION' -X 'main.buildDate=$NOW' -X 'main.goVersion=$GO_VERSION' -X 'main.gitVersion=$GIT_VERSION'"
+version=`git describe --tags $(git rev-list --tags --max-count=1)`
+now=`TZ=Asia/Shanghai date "+%Y%m%d-%H%M"`
+go_version=`go version|awk '{print $3,$4}'`
+git_version=`git rev-parse HEAD`
+ldflags="-w -s -X 'main.version=$version' -X 'main.buildDate=$now' -X 'main.goVersion=$go_version' -X 'main.gitVersion=$git_version'"
 
-GOOS=windows GOARCH=amd64 go build -ldflags "$LDFLAGS" -o result/webssh_windows_amd64.exe .
-GOOS=windows GOARCH=386 go build -ldflags "$LDFLAGS" -o result/webssh_windows_386.exe .
-GOOS=linux GOARCH=amd64 go build -ldflags "$LDFLAGS" -o result/webssh_linux_amd64 .
-GOOS=linux GOARCH=arm64 go build -ldflags "$LDFLAGS" -o result/webssh_linux_arm64 .
-GOOS=darwin GOARCH=amd64 go build -ldflags "$LDFLAGS" -o result/webssh_darwin_amd64 .
-GOOS=darwin GOARCH=arm64 go build -ldflags "$LDFLAGS" -o result/webssh_darwin_arm64 .
+GOOS=windows GOARCH=amd64 go build -ldflags "$ldflags" -o result/webssh_windows_amd64.exe .
+GOOS=windows GOARCH=386 go build -ldflags "$ldflags" -o result/webssh_windows_386.exe .
+GOOS=linux GOARCH=amd64 go build -ldflags "$ldflags" -o result/webssh_linux_amd64 .
+GOOS=linux GOARCH=arm64 go build -ldflags "$ldflags" -o result/webssh_linux_arm64 .
+GOOS=darwin GOARCH=amd64 go build -ldflags "$ldflags" -o result/webssh_darwin_amd64 .
+GOOS=darwin GOARCH=arm64 go build -ldflags "$ldflags" -o result/webssh_darwin_arm64 .
 
 if [[ $# == 0 ]];then
     cd result
 
-    UPLOAD_ITEM=($(ls -l|awk '{print $9}'|xargs -r))
+    upload_item=($(ls -l|awk '{print $9}'|xargs -r))
 
-    for ITEM in ${UPLOAD_ITEM[@]}
+    for item in ${upload_item[@]}
     do
-        upload $ITEM
+        upload $item
     done
 
     echo "upload completed!"
 
-    cd $SHELL_PATH
+    cd $shell_path
 
     rm -rf result
 fi
--- buildx.sh
+++ buildx.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-VERSION=`git describe --tags $(git rev-list --tags --max-count=1)`
-NOW=`TZ=Asia/Shanghai date "+%Y%m%d-%H%M"`
-GO_VERSION=`go version|awk '{print $3,$4}'`
-GIT_VERSION=`git rev-parse HEAD`
-LDFLAGS="-w -s -X 'main.version=$VERSION' -X 'main.buildDate=$NOW' -X 'main.goVersion=$GO_VERSION' -X 'main.gitVersion=$GIT_VERSION'"
+version=`git describe --tags $(git rev-list --tags --max-count=1)`
+now=`TZ=Asia/Shanghai date "+%Y%m%d-%H%M"`
+go_version=`go version|awk '{print $3,$4}'`
+git_version=`git rev-parse HEAD`
+ldflags="-w -s -X 'main.version=$version' -X 'main.buildDate=$now' -X 'main.goVersion=$go_version' -X 'main.gitVersion=$git_version'"
 
-gox -output="docker/result/webssh_{{.OS}}_{{.Arch}}" -ldflags="$LDFLAGS" -os="linux"
+gox -output="docker/result/webssh_{{.OS}}_{{.Arch}}" -ldflags="$ldflags" -os="linux"
 
 cd docker
-docker buildx build --platform linux/arm64,linux/amd64,linux/arm,linux/386,linux/ppc64le,linux/s390x -t jrohy/webssh:${VERSION} . --push
+docker buildx build --platform linux/arm64,linux/amd64,linux/arm,linux/386,linux/ppc64le,linux/s390x -t jrohy/webssh:${version} . --push
 docker buildx build --platform linux/arm64,linux/amd64,linux/arm,linux/386,linux/ppc64le,linux/s390x -t jrohy/webssh . --push
 docker buildx build --platform linux/arm64 -t jrohy/webssh:arm64 . --push
